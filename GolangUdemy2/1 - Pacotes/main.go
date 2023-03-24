3418ca155d70c8abd137ff8b49de4b74fcc60a69
Jennyads <jennycristina.1998@gmail.com>
2023-03-24 17:49:00 -0300

    update

--- GolangUdemy2/1 - Pacotes/main.go
+++ GolangUdemy2/1 - Pacotes/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"modulo/auxiliar"
-	"fmt")
+	"fmt"
+	"github.com/badoux/checkmail" //dependencia externa
+)
 
 func main(){
 
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
 	//auxiliar.escrever2() //aqui não pode ser exportada por conta de ser pacote diferente(letra minúscula)
+	erro := checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(erro)
 }
 
